772ae2c9178a8f22c3860b6d0162693780f59fd0
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:50:01 +0700

    feat: add operator materials

---
+++ operator/operator.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b); //tamabah
+console.log(a - b); //kurang
+console.log(a * b); //kali
+console.log(a / b); //bagi
+console.log(a % b); //modulo 
+
+// Nah, yang terakhir ada operator %. 
+// Walaupun simbolnya terlihat seperti persen, di JavaScript operator ini disebut modulus, 
+// yaitu untuk mendapatkan sisa dari hasil pembagian."
+
+// "Contohnya 10 dibagi 5 hasilnya 2 dan tidak memiliki sisa, sehingga hasil modulusnya adalah 0.
+
