c2a7ee9cbe5f66d8a578647ba6b3d0a96e1b774a
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:49:49 +0700

    feat: add let materials

---
+++ let/let.js
@@ -0,0 +1,15 @@
+// let umur = 20;
+// console.log(umur);
+
+// umur = 21;
+// console.log(umur);
+
+let umur = 20;
+umur = 21;
+
+console.log(umur);
+
+// Kalau kita tampilkan sebelum dan sesudah perubahan, 
+// kita bisa melihat bahwa nilainya memang berubah dari 20 menjadi 21.
+
+// let = "Saya mau membuat sebuah variabel yang nilainya masih boleh berubah."
