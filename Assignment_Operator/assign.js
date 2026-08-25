6fb449e75f4770e4d3cb1042ba898bc119f9660e
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:50:05 +0700

    feat: add Assignment_Operator materials

---
+++ Assignment_Operator/assign.js
@@ -0,0 +1,8 @@
+let skor = 50;
+skor += 10;
+
+console.log(skor);
+
+// Nah, sekarang kita punya skor awal 50. Saya ingin menambahkan 10 ke skor tersebut. 
+// Kita sebenarnya bisa menulis skor = skor + 10, tetapi JavaScript menyediakan cara yang lebih singkat yaitu skor += 10. 
+// Jadi maksudnya adalah ambil nilai skor yang sekarang, tambahkan 10, kemudian simpan kembali hasilnya ke variabel skor
