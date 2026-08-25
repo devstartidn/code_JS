868ee2d24ab5fef3f59da14e5bc4f3da8afd9334
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:50:45 +0700

    feat: add function materials

---
+++ function/function.js
@@ -0,0 +1,16 @@
+function sapa() {
+    console.log("Halo DevStart!");
+}
+
+sapa();
+sapa();
+
+
+// "Sekarang kita masuk ke function. 
+// Function bisa kita bayangkan seperti sebuah mesin atau sebuah perintah yang kita buat untuk melakukan tugas tertentu."
+
+// "Di sini saya membuat function bernama sapa. Saya menggunakan keyword function, 
+// kemudian memberikan nama sapa, diikuti tanda kurung dan kurung kurawal."
+
+// "Di dalam kurung kurawal ini kita masukkan kode yang ingin dijalankan. 
+// Pada contoh ini, tugas function kita adalah menampilkan tulisan Halo DevStart menggunakan console.log()."
