4a123aa644bc1c112e572719d7a8260b39f97e99
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:49:53 +0700

    feat: add const materials

---
+++ const/const.js
@@ -0,0 +1,10 @@
+const negara = "Indonesia";
+console.log(negara);
+
+
+// Kalau sebelumnya kita menggunakan let, sekarang kita menggunakan const. 
+// const digunakan ketika kita memiliki sebuah variabel yang nilainya tidak ingin kita ubah. 
+// Di sini kita membuat variabel negara dengan nilai Indonesia."
+
+// Ketika kita coba memberikan nilai baru yaitu Malaysia, JavaScript akan menghasilkan error. 
+// Jadi berbeda dengan let, nilai yang sudah diberikan pada variabel const tidak dapat kita ganti dengan nilai baru.
