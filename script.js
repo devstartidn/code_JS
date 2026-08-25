61e06300bd3fa88a2118725cefe78ea043de61c0
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:48:30 +0700

    feat: add basic script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// let umur = 15;
+// let memilikiKTP = true;
+
+// console.log(umur >=17 && memilikiKTP);
+
+// let memilikiMember = false
+// let dapetPromo = true
+
+// console.log(memilikiMember || dapetPromo)
+
+// let aktif = true;
+
+// console.log (aktif);
+
+let nilai = 50;
+
+if (nilai >= 75) {
+    console.log("lulus");
+} else {
+    console.log("tidak lulus");
+}
