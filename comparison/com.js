61db0f28c073d4fdf680ea927fba01afc7c2305b
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:50:09 +0700

    feat: add comparison materials

---
+++ comparison/com.js
@@ -0,0 +1,33 @@
+let nilai = 80;
+console.log(nilai >= 75);
+
+
+// Comparison operator digunakan untuk membandingkan dua nilai.
+// Hasil dari perbandingan tersebut selalu berupa Boolean, yaitu:
+
+// true
+// false
+
+// >
+// Lebih besar
+// 80 > 75
+
+// <
+// Lebih kecil
+// 60 < 75
+
+// >=
+// Lebih besar atau sama
+// 80 >= 75
+
+// <=
+// Lebih kecil atau sama
+// 60 <= 75
+
+// ===
+// Sama nilai dan tipe
+// 80 === 80
+
+// !==
+// Tidak sama nilai atau tipe
+// 80 !== 75
