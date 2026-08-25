2e9f16e353d259349dbaf4338ffea7236abaeb7a
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:50:41 +0700

    feat: add ifelse materials

---
+++ ifelse/ifelse.js
@@ -0,0 +1,14 @@
+let nilai = 80;
+
+if (nilai >= 75) {
+    console.log("Lulus");
+} else {
+    console.log("Tidak Lulus");
+}
+
+// "Tadi kita sudah belajar comparison operator. 
+// JavaScript bisa membandingkan apakah nilai 80 lebih besar atau sama dengan 75 dan menghasilkan true atau false. 
+// Sekarang kita gunakan hasil tersebut untuk mengambil keputusan."
+
+// "Kalau kondisinya benar, JavaScript menjalankan bagian if. 
+// Kalau kondisinya salah, JavaScript menjalankan bagian else."
