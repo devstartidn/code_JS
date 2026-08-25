d71fe999ae6b3e0f7cc9330e02fdaa997bb16687
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:49:44 +0700

    feat: add variabel materials

---
+++ variabel/vat.js
@@ -0,0 +1,17 @@
+let nama = "Sulthan";
+let umur = 20;
+
+console.log(nama);
+console.log(umur);
+
+// Sekarang kita masuk ke variabel. 
+// Variabel itu bisa kita bayangkan seperti sebuah wadah untuk menyimpan data. 
+// Misalnya saya punya wadah bernama nama, kemudian saya masukkan data Sulthan ke dalamnya. 
+// Lalu saya punya wadah bernama umur, dan saya masukkan angka 20."
+
+// "Di JavaScript kita bisa membuat wadah tersebut menggunakan let. 
+// Jadi let nama = 'Sulthan' artinya kita membuat variabel bernama nama dan memberikan nilai Sulthan ke dalamnya."
+
+// "Setelah datanya disimpan, kita bisa menggunakan variabel tersebut berkali-kali. 
+// Contohnya di sini saya menggunakan console.log(nama), 
+// maka JavaScript akan mengambil isi dari variabel nama dan menampilkannya di Console
