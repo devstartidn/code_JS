76af88942436e5ba991aabcf9116099366d06531
SulthanAndyno <sulthandyno@gmail.com>
2026-08-25 13:49:57 +0700

    feat: add tipe_data materials

---
+++ tipe_data/tipe.js
@@ -0,0 +1,11 @@
+let nama = "Sulthan"; //string
+let umur = 20; //number
+let mahasiswa = true; //boolean
+let alamat = null; //null
+let jurusan; //undifined
+
+console.log(nama);
+console.log(umur);
+console.log(mahasiswa);
+console.log(alamat);
+console.log(jurusan);
